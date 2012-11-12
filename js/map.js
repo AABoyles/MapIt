674c38d40f11a47caa6fc3e511d690a79354dbf6
AABoyles <AABoyles@gmail.com>
2012-11-11 21:02:17 -0500

    Killed WMS, added DrawingTools
    *Added Drawing Tools
    *Pulled out the WMS

--- js/map.js
+++ js/map.js
@@ -3,19 +3,23 @@ var map;
 function init() {
   map = new OpenLayers.Map('map');
   map.addLayers([
-  	new OpenLayers.Layer.OSM("Open Street Map"),
-		new OpenLayers.Layer.WMS( "World Map Service",
-			"http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'}),
-		new OpenLayers.Layer.Google("Google Streets"),
+    new OpenLayers.Layer.OSM("Open Street Map"),
+    /*TODO: WMS uses a default projection ("EPSG:4326") which doesn't have a direct mapping to the Spherical Mercator of OSM 
+     * and Google. Figure out how to project it.*/
+    //new OpenLayers.Layer.WMS("Web Map Service","http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'}),
+    new OpenLayers.Layer.Google("Google Streets"),
   ]);
-  map.addControl(new OpenLayers.Control.LayerSwitcher());
-  //map.setCenter(new OpenLayers.LonLat(0, 0).transform(
-  	//new OpenLayers.Projection("EPSG:4326"), map.getProjectionObject()), 1);
+  var drawingLayer = new OpenLayers.Layer.Vector("Drawing Layer")
+  map.addLayer(drawingLayer);
+  map.addControls([
+  	new OpenLayers.Control.LayerSwitcher(),
+  	new OpenLayers.Control.EditingToolbar(drawingLayer)
+  ]);
   map.zoomToMaxExtent();
 }
 
 function addUrl(fileurl) {
-	map.addLayer(new OpenLayers.Layer.Vector("KML", {
+  map.addLayer(new OpenLayers.Layer.Vector("KML", {
     strategies: [new OpenLayers.Strategy.Fixed()],
     protocol: new OpenLayers.Protocol.HTTP({
       url: fileurl,
